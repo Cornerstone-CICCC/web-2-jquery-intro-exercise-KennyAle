424916069582ff76a009c09e2183e767da895b01
KennyAle <kennyviver@gmail.com>
2024-11-25 12:05:40 -0800

    Exercise

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-$(function() {
-  // YOUR CODE HERE
+$(function () {
+  $("#toggleButton").on('click', () => {
+    $("#myParagraph").toggle()
+  })
+  $("#colorButton").on('click', () => {
+    $("#colorDiv").css('background-color', 'yellow')
+  })
+  $("#addClassButton").on('click', () => {
+    $("#classDiv").addClass('grey')
+  })
+  $("#removeClassButton").on('click', () => {
+    $("#classDiv").removeClass('grey')
+  })
+  $("#fadeInButton").on('click', () => {
+    $("#fadeDiv").fadeIn()
+  })
+  $("#fadeOutButton").on('click', () => {
+    $("#fadeDiv").fadeOut()
+  })
+  $("#slideUpButton").on('click', () => {
+    $("#slideDiv").slideUp()
+  })
+  $("#slideDownButton").on('click', () => {
+    $("#slideDiv").slideDown()
+  })
 })
